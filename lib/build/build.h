c7cfa5decafe3424ffc2ff7244ade8d8fd8827f5
mdarius31 <darius31murariu@gmail.com>
2024-11-17 00:18:02 +0200

    build system progress

---
+++ lib/build/build.h
@@ -0,0 +1,12 @@
+#ifndef BUILD_SYSTEM
+#define BUILD_SYSTEM
+
+#include <stdlib.h>
+
+struct program {
+
+};
+
+
+
+#endif //BUILD_SYSTEM
--- threeNPlusOne/build.c
+++ threeNPlusOne/build.c
@@ -1,7 +1,12 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "../lib/build/build.h"
 
-// future completely cross-platform build script
 int main(void) {
- return 0;
+  system("rm threeNPlusOne.o threeNPlusOne.x86_64");
+
+  system("cc -c -o threeNPlusOne.o -m32 -Wextra -Wall -Werror -ansi --std=c99 "
+         "../lib/threeNPlusOne/threeNPlusOne.c");
+  system("cc -o threeNPlusOne.x86_64 -m32 -Wextra -Wall -Werror -ansi "
+         "--std=c99 threeNPlusOne.c threeNPlusOne.o");
+  system("rm threeNPlusOne.o");
+  return 0;
 }
