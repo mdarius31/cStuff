74f012e46898d5107dee1905829d53721aa23e02
mdarius31 <darius31murariu@gmail.com>
2025-01-05 14:57:49 +0200

    raylib lib

--- lib/build/build.h
+++ lib/build/build.h
@@ -1,12 +1,61 @@
 #ifndef BUILD_SYSTEM
 #define BUILD_SYSTEM
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct Target {
+  char *name;
+  struct Target *dependencies;
+  char **cFlags;
+
+} Target;
+
+int build(struct Target *t);
+
+#ifdef __cplusplus
+}
+#endif
+
+// BUILD_SYSTEM
+#endif
+
+#ifdef BUILD_SYSTEM_IMPLEMENTATION
+
+// in case you hate the standard libraries
+#ifndef BUILD_SYSTEM_IMPLEMENTATION_DONT_USE_STDLIB
+
+#include <stdio.h>
 #include <stdlib.h>
 
-struct program {
+// BUILD_SYSTEM_IMPLEMENTATION_DONT_USE_STDLIB
+#endif
 
-};
+// used to ignore the unused parameter warnings
+#define UNUSED(x) (void)(x)
 
+int build(struct Target *t) {
 
+  printf("%s\n", t->name);
 
-#endif //BUILD_SYSTEM
+  unsigned int depsLen = sizeof(t->dependencies) / sizeof(t->dependencies[0]);
+
+  // for (unsigned int i = 0; i < depsLen; i++) {
+  //   printf("%s ", t->dependencies[i].name);
+  // }
+  printf("\n");
+  for (unsigned int i = 0; t->cFlags[i] != NULL; i++) {
+    printf("%s ", t->cFlags[i]);
+  }
+
+  system("cc -c -o threeNPlusOne.o -m32 -Wextra -Wall -Werror -ansi --std=c99 "
+         "../lib/threeNPlusOne/threeNPlusOne.c");
+  system("cc -o threeNPlusOne.x86 -m32 -Wextra -Wall -Werror -ansi --std=c99 "
+         "threeNPlusOne.c threeNPlusOne.o");
+  printf("\nbuild complete\n");
+  return 0;
+}
+
+// BUILD_SYSTEM_IMPLEMENTATION
+#endif
--- threeNPlusOne/build.c
+++ threeNPlusOne/build.c
@@ -1,12 +1,23 @@
+#define BUILD_SYSTEM_IMPLEMENTATION
 #include "../lib/build/build.h"
 
+// recommended build process
+// cc -o build.x86 -m32 -Wextra -Wall -Werror -ansi --std=c99 build.c
 int main(void) {
-  system("rm threeNPlusOne.o threeNPlusOne.x86_64");
 
-  system("cc -c -o threeNPlusOne.o -m32 -Wextra -Wall -Werror -ansi --std=c99 "
-         "../lib/threeNPlusOne/threeNPlusOne.c");
-  system("cc -o threeNPlusOne.x86_64 -m32 -Wextra -Wall -Werror -ansi "
-         "--std=c99 threeNPlusOne.c threeNPlusOne.o");
-  system("rm threeNPlusOne.o");
-  return 0;
+  char *cFlags[] = {"-m32",  "-Wextra",   "-Wall", "-Weerror",
+                    "-ansi", "--std=c99", NULL};
+
+  struct Target dependencies[2] = {
+      {.name = "threeNPlusOne.c", .dependencies = NULL, .cFlags = cFlags},
+      {.name = "threeNPlusOne.o", .dependencies = NULL, .cFlags = cFlags},
+  };
+
+  struct Target t = {.name = "threeNPlusOne.x86",
+                     .dependencies = dependencies,
+                     .cFlags = cFlags};
+
+  int result = build(&t);
+
+  return result;
 }
