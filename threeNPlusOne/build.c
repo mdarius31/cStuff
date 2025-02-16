648d4250e3563f64dd4c0c402cdec2953342bef3
mdarius31 <darius31murariu@gmail.com>
2025-02-16 20:30:00 +0200

    simplify

--- threeNPlusOne/build.c
+++ threeNPlusOne/build.c
@@ -1,11 +1,13 @@
 #define MDB_BUILD_SYSTEM_IMPLEMENTATION
 #include "../lib/build/build.h"
+#include <assert.h>
+#include <printf.h>
 
 int main(void) {
 		char *compiler = "cc";
 		char *namingFlag = "-o";
 
-		mdb_Flag flags[]= {"-static", "-m32",  "-Wextra",   "-Wall", "-Werror",
+		mdb_Flag flags[] = {"-static", "-m32",  "-Wextra",   "-Wall", "-Werror",
                    "-ansi", "--std=c99", NULL};
 		mdb_Flag depFlags[] = {"-c", "-static", "-m32",  "-Wextra",   "-Wall", "-Werror",
                    "-ansi", "--std=c99", NULL};
@@ -13,13 +15,11 @@ int main(void) {
 		mdb_Target *final = mdb_newTarget("threeNPlusOne.x86_64", "threeNPlusOne.c", compiler, namingFlag, flags);
 		mdb_addNewDep(final, "threeNPlusOne.o", "../lib/threeNPlusOne/threeNPlusOne.c", compiler, namingFlag, depFlags);
 
-		int resultOfComps = mdb_buildTarget(final);
+		assert(mdb_buildTarget(final) == 0);
 
 		char *testCmd = "./threeNPlusOne.x86_64 10";
 
 		printf("test command: %s\n", testCmd);
 
-		resultOfComps += system(testCmd);
-
-  return resultOfComps;
+  return system(testCmd);
 }
