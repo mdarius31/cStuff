0bcb4aa07d2db2ca5a66cfd100f7a10a789b9e9d
mdarius31 <darius31murariu@gmail.com>
2025-02-16 16:39:40 +0200

    better build script

--- threeNPlusOne/build.c
+++ threeNPlusOne/build.c
@@ -5,15 +5,21 @@ int main(void) {
 		char *compiler = "cc";
 		char *namingFlag = "-o";
 
-		mdb_Flag flags[]= {"-m32",  "-Wextra",   "-Wall", "-Werror",
+		mdb_Flag flags[]= {"-static", "-m32",  "-Wextra",   "-Wall", "-Werror",
                    "-ansi", "--std=c99", NULL};
-		mdb_Flag depFlags[] = {"-c", "-m32",  "-Wextra",   "-Wall", "-Werror",
+		mdb_Flag depFlags[] = {"-c", "-static", "-m32",  "-Wextra",   "-Wall", "-Werror",
                    "-ansi", "--std=c99", NULL};
 
 		mdb_Target *final = mdb_newTarget("threeNPlusOne.x86_64", "threeNPlusOne.c", compiler, namingFlag, flags);
 		mdb_addNewDep(final, "threeNPlusOne.o", "../lib/threeNPlusOne/threeNPlusOne.c", compiler, namingFlag, depFlags);
 
-		mdb_buildTarget(final);
+		int resultOfComps = mdb_buildTarget(final);
 
-  return 0;
+		char *testCmd = "./threeNPlusOne.x86_64 10";
+
+		printf("test command: %s\n", testCmd);
+
+		resultOfComps += system(testCmd);
+
+  return resultOfComps;
 }
