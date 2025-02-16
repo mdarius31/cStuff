bc9a09257c4c5b1483f4a908595a1b2bd1ff8f88
mdarius31 <darius31murariu@gmail.com>
2025-02-16 16:39:11 +0200

    getting rid of useless files

--- threeNPlusOne/build.sh
+++
@@ -1,5 +0,0 @@
-# temporary build script
-rm threeNPlusOne.o threeNPlusOne.x86_64
-cc -c -o threeNPlusOne.o -m32 -Wextra -Wall -Werror -ansi --std=c99 ../lib/threeNPlusOne/threeNPlusOne.c
-cc -o threeNPlusOne.x86_64 -m32 -Wextra -Wall -Werror -ansi --std=c99 threeNPlusOne.c threeNPlusOne.o
-./threeNPlusOne.x86_64 10
