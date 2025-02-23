3e446dc95c62c1443cde0108135a95e22299e794
mdarius31 <darius31murariu@gmail.com>
2025-02-23 19:17:33 +0200

    using different flags for deps and final

--- threeNPlusOneRaylib/build.c
+++ threeNPlusOneRaylib/build.c
@@ -4,12 +4,28 @@
 int main(void) {
 	char *compiler = "cc";
 	char *namingFlag = "-o";
-	mdb_Flag flags[] = {
-		"-lm", "-m32", "-Wall", "-Wextra", NULL
+	const mdb_Flag baseFlags[] = {
+ "-g","-m32", "-Wextra", "-Wall", "-Werror", "-ansi", "--std=c99", NULL
 	};
 
+	const unsigned int baseFlagsSize = sizeof(baseFlags);
+	const unsigned int mdb_FlagSize = sizeof(mdb_Flag);
+
+	const unsigned int flagsSize = baseFlagsSize + mdb_FlagSize;
+
+	mdb_Flag *flags = malloc(flagsSize);
+	memcpy(flags, baseFlags, sizeof(baseFlags));
+	flags[(baseFlagsSize / mdb_FlagSize) - 1] =  "-lm";
+
+	const unsigned int depFlagsSize = flagsSize;
+
+	mdb_Flag *depFlags = malloc(depFlagsSize);
+	memcpy(depFlags, baseFlags, sizeof(baseFlags));
+
+	depFlags[(baseFlagsSize / mdb_FlagSize) - 1] =  "-c";
+
 	mdb_Target *final = mdb_newTarget("threeNPlusOneRaylib.x86_64", "threeNPlusOneRaylib.c", compiler, namingFlag, flags);
-	mdb_addNewDep(final, "threeNPlusOne.o", "../lib/threeNPlusOne/threeNPlusOne.c", compiler, namingFlag, (mdb_Flag[]) {"-c", "-m32",NULL});
+	mdb_addNewDep(final, "threeNPlusOne.o", "../lib/threeNPlusOne/threeNPlusOne.c", compiler, namingFlag, depFlags);
 	mdb_addCompiledDep(final, "../lib/raylib/src/raudio.o");
 	mdb_addCompiledDep(final, "../lib/raylib/src/rglfw.o");
 	mdb_addCompiledDep(final, "../lib/raylib/src/rshapes.o");
@@ -20,5 +36,8 @@ int main(void) {
 	mdb_addCompiledDep(final, "../lib/raylib/src/utils.o");
 	mdb_buildTarget(final);
 
+
+	free(flags);
+	free(depFlags);
 	return 0;
 }
