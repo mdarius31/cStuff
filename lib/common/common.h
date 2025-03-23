e5b20e7ad2ea7dd7f1e1d2cde4f0dd8b32394fe5
mdarius31 <darius31murariu@gmail.com>
2025-03-23 21:39:06 +0200

    upgraded UNUSED macro that warns what variable on what line is marked as unused

--- lib/common/common.h
+++ lib/common/common.h
@@ -1,4 +1,9 @@
 #pragma once
 
 // used to ignore the unused parameter warnings
-#define UNUSED(x) (void)(x)
+#define UNUSED(x) ({\
+ (void)(x);\
+ printf("UNUSED VAR: %s ON LINE %u\n", #x, __LINE__);\
+ fflush(stdout);\
+})
+
